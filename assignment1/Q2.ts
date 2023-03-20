634ca6e7eeb2b8445e153bdd793f85205b22f325
Patil369 <128421373+Patil369@users.noreply.github.com>
2023-03-21 01:24:02 +0530

    Update Q2.ts

--- assignment1/Q2.ts
+++ assignment1/Q2.ts
@@ -1,1 +1,19 @@
+function Area( Rad : number , Pi : number ) : number
+{
+    let Ans : number = 0
 
+    Ans = Pi * ( Rad * Rad )
+
+    return Ans
+
+}
+
+var Rad : number = 5
+
+var Pi : number = 3.14
+
+var Ans : number = 0
+
+Ans = Area( Rad, Pi ) 
+
+console.log ( "Area of Circle : " + Ans )
