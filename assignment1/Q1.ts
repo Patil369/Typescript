b4da596f50943cdf7407b15e8daa21ee48634ec2
Patil369 <128421373+Patil369@users.noreply.github.com>
2023-03-21 01:20:51 +0530

    Update Q1.ts

--- assignment1/Q1.ts
+++ assignment1/Q1.ts
@@ -1,1 +1,27 @@
+function Maximum(A : number, B : number, C : number) : number
+{
+    if(A > B && A > C)
+    {
+        return A
+    }
+    else if(B > A && B > C)
+    {
+        return B
+    }
+    else
+    {
+        return C
+    }
+}
 
+var No1 : number = 23
+
+var No2 : number = 89
+
+var No3 : number = 6
+
+var Ret : number = 0
+
+Ret = Maximum(No1, No2,No3)
+
+console.log( "Maximum number is:" + Ret )
