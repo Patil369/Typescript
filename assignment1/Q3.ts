7681a8537bb2b3a5b245bb8099c04a27ade91c04
Patil369 <128421373+Patil369@users.noreply.github.com>
2023-03-29 23:21:59 +0530

    Update Q3.ts

--- assignment1/Q3.ts
+++ assignment1/Q3.ts
@@ -2,7 +2,7 @@ function Factor (No1 : number) : void
 {
     let cnt : number = 0
 
-    for( cnt = 1; cnt  < N1; cnt++ )
+    for( cnt = 1; cnt  < No1; cnt++ )
     {
         if( No1 % cnt == 0 )
         console.log( cnt )
